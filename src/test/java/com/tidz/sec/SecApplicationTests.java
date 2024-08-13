aacd6e01661ad30acdb2688051444370b7457a83
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-13 10:06:59 -0300

    first commit

---
+++ src/test/java/com/tidz/sec/SecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
